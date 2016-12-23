3e13bec3a2fd4b3e3312f961be3d80f3734d7eaf
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-12-23 14:36:19 -0600

    iOS Checkbox

--- iOS.Controls/CheckboxRenderer.cs
+++ iOS.Controls/CheckboxRenderer.cs
@@ -8,6 +8,7 @@ using Messier16.Forms.iOS.Controls.Native.Checkbox;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 using FormsCheckbox = Messier16.Forms.Controls.Checkbox;
+using FormsCheckboxCheckedChangedEventArgs = Messier16.Forms.Controls.CheckedChangedEventArgs;
 using Messier16.Forms.iOS.Controls;
 
 [assembly: ExportRenderer(typeof(FormsCheckbox), typeof(CheckboxRenderer))]
@@ -23,93 +24,100 @@ namespace Messier16.Forms.iOS.Controls
             var temp = DateTime.Now;
         }
 
-        /// <summary>
-        /// Handles the Element Changed event
-        /// </summary>
-        /// <param name="e">The e.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+                Control.ValueChanged -= ControlValueChanged;
+
+            base.Dispose(disposing);
+        }
+
         protected override void OnElementChanged(ElementChangedEventArgs<FormsCheckbox> e)
         {
-            if (e.OldElement != null || Element == null) return;
-
-            if (Control == null)
-            {
-                // Instantiate the native control and assign it to the Control property
-                var width = 40d;
-                if (Element.WidthRequest >= 0)
-                {
-                    width = Element.WidthRequest;
-                }
-                var checkBox = new Native.Checkbox.Checkbox(new CGRect(0, 0, width, width))
-                {
-                    //Bounds = new CGRect(0, 0, width, width)
-                };
-                SetNativeControl(checkBox);
-            }
-
             if (e.OldElement != null)
-            {
-                // Unsubscribe from event handlers and cleanup any resources
-                Control.CheckedChanged -= Control_CheckedChanged;
-            }
+                e.OldElement.CheckedChanged -= OnElementCheckedChanged;
 
             if (e.NewElement != null)
             {
-                // Configure the control and subscribe to event handlers
-                Control.CheckedChanged += Control_CheckedChanged;
+                if (Control == null)
+                {
+                    // Instantiate the native control and assign it to the Control property
+                    var width = 140d;
+                    if (Element.WidthRequest > 0)
+                    {
+                        width = Element.WidthRequest;
+                    }
+                    var checkBox = new Native.Checkbox.Checkbox(new CGRect(0, 0, width, width))
+                    {
+                    };
+                    SetNativeControl(checkBox);
+                    Control.ValueChanged += ControlValueChanged;
+                }
+                Control.Checked = Element.Checked;
+                e.NewElement.CheckedChanged += OnElementCheckedChanged;
 
-                Control.Enabled = e.NewElement.IsEnabled;
-                Control.Checked = e.NewElement.Checked;
-                if (e.NewElement.CheckboxBackgroundColor != Color.Default)
-                    Control.CheckboxBackgroundColor = e.NewElement.CheckboxBackgroundColor.ToUIColor();
-
-                if (e.NewElement.TickColor != Color.Default)
-                    Control.TickColor = e.NewElement.TickColor.ToUIColor();
             }
+
+            //if (e.NewElement != null)
+            //{
+            //    // Configure the control and subscribe to event handlers
+            //    Control.CheckedChanged += Control_CheckedChanged;
+
+            //    Control.Enabled = e.NewElement.IsEnabled;
+            //    Control.Checked = e.NewElement.Checked;
+            //    if (e.NewElement.CheckboxBackgroundColor != Color.Default)
+            //        Control.CheckboxBackgroundColor = e.NewElement.CheckboxBackgroundColor.ToUIColor();
+
+            //    if (e.NewElement.TickColor != Color.Default)
+            //        Control.TickColor = e.NewElement.TickColor.ToUIColor();
+            //}
 
             base.OnElementChanged(e);
         }
 
-
-
-
-        private void Control_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        private void OnElementCheckedChanged(object sender, FormsCheckboxCheckedChangedEventArgs e)
         {
-            Element.Checked = e.Checked;
+            Control.Checked = Element.Checked;
         }
 
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        private void ControlValueChanged(object sender, EventArgs e)
         {
-            if (Element == null || Control == null) return;
-
-            if (e.PropertyName == nameof(Element.IsEnabled))
-            {
-                Control.Enabled = Element.IsEnabled;
-            }
-            else if (e.PropertyName == nameof(Element.Checked))
-            {
-                Control.Checked = Element.Checked;
-            }
-            else if (e.PropertyName == nameof(Element.TickColor))
-            {
-                Control.TickColor = Element.TickColor.ToUIColor();
-            }
-            else if (e.PropertyName == nameof(Element.BackgroundColor))
-            {
-                Control.CheckboxBackgroundColor = Element.BackgroundColor.ToUIColor();
-            }
-            else if (e.PropertyName == nameof(Element.IsVisible))
-            {
-                Control.Hidden = !Element.IsVisible;
-                Control.SetNeedsDisplay();
-            }
-            else
-            {
-                base.OnElementPropertyChanged(sender, e);
-            }
-            //else if (e.PropertyName == nameof(Element.IsVisible))
-            //{
-            //	Control.Hidden = !Element.IsVisible;
-            //}
+            ((IElementController)Element).SetValueFromRenderer(FormsCheckbox.CheckedProperty, Control.Checked);
         }
+        
+        //protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    if (Element == null || Control == null) return;
+
+        //    if (e.PropertyName == nameof(Element.IsEnabled))
+        //    {
+        //        Control.Enabled = Element.IsEnabled;
+        //    }
+        //    else if (e.PropertyName == nameof(Element.Checked))
+        //    {
+        //        Control.Checked = Element.Checked;
+        //    }
+        //    else if (e.PropertyName == nameof(Element.TickColor))
+        //    {
+        //        Control.TickColor = Element.TickColor.ToUIColor();
+        //    }
+        //    else if (e.PropertyName == nameof(Element.BackgroundColor))
+        //    {
+        //        Control.CheckboxBackgroundColor = Element.BackgroundColor.ToUIColor();
+        //    }
+        //    else if (e.PropertyName == nameof(Element.IsVisible))
+        //    {
+        //        Control.Hidden = !Element.IsVisible;
+        //        Control.SetNeedsDisplay();
+        //    }
+        //    else
+        //    {
+        //        base.OnElementPropertyChanged(sender, e);
+        //    }
+        //    //else if (e.PropertyName == nameof(Element.IsVisible))
+        //    //{
+        //    //	Control.Hidden = !Element.IsVisible;
+        //    //}
+        //}
     }
 }
